e3538c30b71a899e45e769711989ab9c62bea3b3
Stijnson-nl <info@stijnson.nl>
2026-09-03 14:04:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const client = new Client({
   partials: [Partials.Message, Partials.Channel, Partials.GuildMember],
 });
 
-const TOKEN = 'MTEwNjkyODM4Mjc1MjYwMDIwNA.GY-JAv.luwXMGRyvZlgasNIFcNALgZcwl0UkrC1EFjBGg'; // Replace with your bot token
+const TOKEN = process.env.DISCORD_TOKEN; // Replace with your bot token
 const CLIENT_ID = '1106928382752600204'; // Replace with your bot's client ID
 const GUILD_ID = '1394401573240574154'; // Replace with your test server's guild ID
 
